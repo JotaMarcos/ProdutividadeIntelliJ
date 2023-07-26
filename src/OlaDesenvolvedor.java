5235d390a8bc3ce668b83a3cca154b80d213e048
João Marcos <f509451@corp.caixa.gov.br>
2023-07-26 18:15:36 -0300

    Realizado validação de produtividade e configuração do arquivo .editorconfig

---
+++ src/OlaDesenvolvedor.java
@@ -0,0 +1,11 @@
+public class OlaDesenvolvedor {
+
+    public static void main(String[] args) {
+        int total = 10;
+        boolean pendente = true;
+
+        for (int i = total; i > 0; i--) {
+            
+        }
+    }
+}
